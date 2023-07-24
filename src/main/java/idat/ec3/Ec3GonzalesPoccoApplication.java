953e720b230dc0185ffc10e9393daea09f93da4b
Martin_83 <107376069+Martin0830@users.noreply.github.com>
2023-07-24 13:27:49 -0500

    Primer Commit

---
+++ src/main/java/idat/ec3/Ec3GonzalesPoccoApplication.java
@@ -0,0 +1,13 @@
+package idat.ec3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec3GonzalesPoccoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ec3GonzalesPoccoApplication.class, args);
+    }
+
+}
